89d1eeb646e0a8aed83f6b641d29923ae269e12a
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-27 22:34:44 +0200

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -18,33 +18,40 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        if (str == null)
-            return "";
-        String str1 = "";
+        String output = "";
         for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            ch = (ch >= 'A' && ch <= 'Z' ? (char) (ch - 'A' + 'a') : ch);
-            str1 = str1 + ch;
+            char c = str.charAt(i);
+            if (c >= 65 && c <= 90) {
+                c += 32;
+            }
+            output += c;
         }
-        return str1;
+        return output;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        if (str1 == null || str2 == null)
-            return false;
         if (str2.length() == 0)
             return true;
-        if (str1.length() == 0)
+        if (str1.length() < str2.length())
+            return false;
+        if (str1.length() == 0 || str2.length() == 0)
             return false;
 
-        str1 = lowerCase(str1);
-        str2 = lowerCase(str2);
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(0)) {
+                int hits = 0;
+                for (int j = 0; j < str2.length(); j++) {
+                    if (i + j >= str1.length())
+                        break;
 
-        for (int i = 0; i <= str1.length() - str2.length(); i++) {
-            String sub = str1.substring(i, i + str2.length());
-            if (sub.equals(str2))
-                return true;
+                    if (str1.charAt(i + j) == str2.charAt(j)) {
+                        hits++;
+                    }
+                }
+                if (hits == str2.length())
+                    return true;
+            }
         }
         return false;
     }
